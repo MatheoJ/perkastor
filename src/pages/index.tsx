cf15f89751244b5fe5a1b5de96e028a148623aad
mayline0104 <mayline.le-polles@insa-lyon.fr>
2023-05-03 11:53:34 +0200

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home: NextPage = () => {
         <div>
           <h1> Perkastor </h1> 
           <b> À qui s’adresse-t-on ? </b>
-          <p> Aux fans de l’histoire et à ceux qui souhaitent la découvrir !Tout le monde a déjà entendu une anecdote historique folle sur sa ville, difficile à trouver dans les sources officielles souvent très exhaustives. À Perkastor, on regroupe ces anecdotes rien que pour vous 😊 </p>
+          <p> Aux fans de l’histoire et à ceux qui souhaitent la découvrir ! Tout le monde a déjà entendu une anecdote historique folle sur sa ville, difficile à trouver dans les sources officielles souvent très exhaustives. À Perkastor, on regroupe ces anecdotes rien que pour vous 😊 </p>
           <b> Comment contribuer ? </b>
           <p> Ce site est collaboratif, n’hésitez pas à rajouter vos propres anecdotes historiques. </p>
           <p> Le projet Perkastor étant open source, on vous invite à participer activement à son développement en vous rendant sur notre Github. </p>
