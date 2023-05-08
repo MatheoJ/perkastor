6be9579ae4e6f02888ae76f18a459b4d8b01f233
yoaaaaaaaann <sabatieryoann@gmail.com>
2023-05-08 14:04:55 +0200

    remove mongodb uri mdr deso antonin

--- migration.js
+++ migration.js
@@ -1,6 +1,5 @@
 const { MongoClient } = require('mongodb');
 
-const MONGO_URI = "mongodb+srv://antoninmarxer:5f3aac58ed65413a8072d625492ec401011eecbaafbc3e27a749e3dcf7ef4560@cluster0.a0nfhg5.mongodb.net/test"
 console.log(MONGO_URI)
 const DATABASE_NAME = 'test'; // Remplacez par le nom de votre base de données
 
