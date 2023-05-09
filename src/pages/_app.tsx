1509163ef406f9f67237f18cca38c3252fa582ea
ClementINSALYON <clement.bousseau@insa-lyon.fr>
2023-05-09 14:55:35 +0200

    design de base

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ import "~/styles/topbar.scss";
 import "~/styles/fact.css";
 import "~/components/batf/Batf.scss";
 import "~/styles/mapStyle.scss";
+import "~/components/searchbar/searchbar.css"; 
 
 import Layout from "../components/layout/layout";
 
