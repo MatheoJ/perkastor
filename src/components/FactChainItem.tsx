8391ae6b9c25b0e6546947701796281404a4ebe9
mayline0104 <mayline.le-polles@insa-lyon.fr>
2023-05-10 16:28:08 +0200

    chainList et factchainitem

--- src/components/FactChainItem.tsx
+++ src/components/FactChainItem.tsx
@@ -1,46 +1,48 @@
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
-import {FactProps} from '../../types/types'
+import { FactChainItem as FactChainItemType } from '@prisma/client'
+import { Fact as FactType } from '@prisma/client'
+import { FactChainItemProps } from '../../types/types'
 
 interface Props {
-    fact: FactProps;
+    item: FactChainItemProps;
 }
 
-const Fact: React.FC<Props> = ( props ) => {
-    const { fact } = props;
+const Fact: React.FC<Props> = (props) => {
+    const { item } = props;
     return (
         <div className="fact">
             <div className="factHead">
                 <div className="factHeadTop">
-                    <h1>{fact.title}</h1>
+                    <h1>{item.title}</h1>
                 </div>
                 <div className="factHeadBottom">
                     <div className="factHeadBottomLeft">
-                        <ul>
-                            <li key={fact.location.id}>{fact.location.name}</li>
-                        </ul>
+                        <li key={item.fact.location.id}>{item.fact.location.name}</li>
                     </div>
                     <div className="factHeadBottomRight">
                         <h2>
-                            19 mai 1952
+                            {item.fact.from}
                         </h2>
                     </div>
                 </div>
             </div>
             <div className="factBody">
                 <div className='content-left'>
-                    <p>{fact.content}</p>
+                    <p>{item.fact.content}</p>
                 </div>
                 <div className='content-right'>
                     <div className="factImage">
                         {/*<Image src={fact.bannerImg} alt="" width={300} height={200} />*/}
                     </div>
                     <ul>
-                        {fact.personsInvolved.map((person) => (
+                        {item.fact.personsInvolved.map((person) => (
                             <li key={person.id}>{person.name}</li>
                         ))}
                     </ul>
-                    
+                    <div className="comment">
+                        <p>{item.comment}</p>
+                    </div>
                 </div>
             </div>
         </div>
