6e5350c0b94a2dd617b838dca84111ad9359cfa8
mayline0104 <mayline.le-polles@insa-lyon.fr>
2023-05-11 14:30:24 +0200

    Update HistoricalFiguresView.tsx

--- src/components/batf/HistoricalFiguresView.tsx
+++ src/components/batf/HistoricalFiguresView.tsx
@@ -72,9 +72,10 @@ const HistoricalFigureView: NextPage<Props> = (props) => {
             </div>
             <div className="historicalFigureBody">
                 <div className='content-left'>
-                    <p>{historicalPerson.shortDesc}</p>
-                    <p>{historicalPerson.content}</p>
+                    {historicalPerson.shortDesc && <p>{historicalPerson.shortDesc.charAt(0).toUpperCase() + historicalPerson.shortDesc.slice(1)}</p>}
+                    {historicalPerson.content && <p>{historicalPerson.content.charAt(0).toUpperCase() + historicalPerson.content.slice(1)}</p>}
                 </div>
+
                 <div className='content-right'>
 
                     {historicalPerson.image && (
